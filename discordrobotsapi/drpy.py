3782589c8e240a4c6555f2194dc8e936a1723098
Ari24-cb24 <56089155+Ari24-cb24@users.noreply.github.com>
2020-06-27 17:05:38 +0200

    Update V.0.4

---
+++ discordrobotsapi/drpy.py
@@ -0,0 +1,96 @@
+import json
+import requests
+import logging
+
+'''
+API for DiscordRobots Bot List
+Python 3.7
+Author: <Author>
+'''
+
+BASE_URL = "https://discordrobots-api.000webhostapp.com/"
+
+logging.basicConfig(format='%(asctime)s - %(process)d: %(message)s', datefmt="%d.%m.%Y %I:%M:%S (%p)")
+
+class TokenIsInvalidException(Exception):
+    pass
+
+class IDIsInvalidException(Exception):
+    pass
+
+class InvalidArgumentException(Exception):
+    pass
+
+class DiscordRobots:
+    """
+    Class for DiscordRobotsAPI in Python
+    Contains functions like connecting to the API, gathering information's and more
+    """
+    def __init__(self):
+        """
+        Initiate the class and sets the default api_url
+        """
+        self.token = None
+        self.api_url = BASE_URL + "conn/test.php?token="
+
+    def connect(self, token: str) -> bool:
+        """
+        Connects to DiscordRobots API
+
+        excepts the following arguments:
+            - the bot's token (str)
+
+        throws a raise TokenIsInvalidException if token is invalid
+
+        returns a bool if the connection was successfully
+        """
+
+        if not type(token) == str:
+            logging.error("Couldn't create a DiscordRobots object. Reason: Token is invalid!")
+            raise TokenIsInvalidException("Token is invalid!")
+
+        self.token = token
+        self.api_url += self.token
+
+        res = requests.get(url=self.api_url, params=dict())
+        data = res.json()
+
+        connected = data['connected']
+        if not connected:
+            logging.error("Failed to connect to DiscordRobots. Reason: Got an invalid Token in DiscordRobots#connect()!")
+            raise TokenIsInvalidException("Token is invalid!")
+        else:
+            logging.info("Successfully connected to DiscordRobotsAPI")
+            return True
+
+    def has_voted(self, user_id: str) -> bool:
+        """
+        Checks if the given user ID (str) has already voted
+
+        excepts a user_id (str)
+
+        throws an IDIsInvalidException if the user ID is invalid
+        throws an TokenIsInvalidException if there's and invalid token
+
+        returns if user has already voted (bool)
+        """
+        if not user_id or not str(user_id).isnumeric():
+            logging.error("Got an invalid User ID in DiscordRobots#has_voted()")
+            raise IDIsInvalidException("User ID is invalid!")
+
+        url = BASE_URL + "votes/test.php?token=" + self.token + "&id=" + user_id
+        res = requests.get(url=url)
+        data = res.json()
+
+        connected = json.dumps(data["connected"], indent=4)
+        if connected == "false":
+            logging.error("Failed to connect to DiscordRobots. Reason: Got an invalid Token in DiscordRobots#has_voted()!")
+            raise TokenIsInvalidException("Got an invalid Token!")
+        else:
+            voted = json.dumps(data["isVoted"], indent=4)
+            logging.info("Successfully gathered information about User having already voted!")
+            return voted == "true"
+
+
+a = DiscordRobots()
+a.connect("7a3j3x5d7x5o7y2w4x0j1x8q3u2p1v4g2e1g6j0u6c5s6c0a0r0e0l2g3m6h5r4j8w2u2g5x4w1i3y5u8y6d4u6z8m7p2z4m6y1t")
