f24eb6a41bbfc0cb1db64702a20155073ee65da7
Ari24-cb24 <56089155+Ari24-cb24@users.noreply.github.com>
2020-07-08 23:39:08 +0200

    V4.03

--- discordrobotsapi/drpy.py
+++ discordrobotsapi/drpy.py
@@ -5,12 +5,19 @@ import logging
 '''
 API for DiscordRobots Bot List
 Python 3.7
-Author: <Author>
+Author: Ari24
+Contributors: Ari24, MauricePascal
+Github: https://github.com/MauricePascal/drpy
 '''
 
 BASE_URL = "https://discordrobots-api.000webhostapp.com/"
-
-logging.basicConfig(format='%(asctime)s - %(process)d: %(message)s', datefmt="%d.%m.%Y %I:%M:%S (%p)")
+LOGGING_LEVELS = {
+    "DEBUG": logging.DEBUG,
+    "INFO": logging.INFO,
+    "WARNING": logging.WARNING,
+    "ERROR": logging.ERROR,
+    "CRITICAL": logging.CRITICAL
+}
 
 class TokenIsInvalidException(Exception):
     pass
@@ -26,12 +33,31 @@ class DiscordRobots:
     Class for DiscordRobotsAPI in Python
     Contains functions like connecting to the API, gathering information's and more
     """
-    def __init__(self):
+    def __init__(self, logging_format='%(asctime)s - %(process)d: %(message)s', logging_datefmt="%d.%m.%Y %I:%M:%S (%p)",
+                 logging_level="WARNING", logging_file=None, logging_mode="a+"):
         """
-        Initiate the class and sets the default api_url
+        Initiate the class and sets the default api_base_url
+
+        positional keywords:
+            - logging_format: sets the format for the logging (default is recommended)
+            - logging_datefmt: sets the time format for the logging (default is recommended)
+            - logging_level: sets the min. level for logging (default and "INFO" is recommended)
+            - logging_file: the logging.log file
+            - logging_mode: the write mode for logging into the file, default is "a+" (recommended) so it appends the current logs to the logs from earlier runs
+
+        can throw:
+            - assertion-error if logging_level isnt a real logging level
         """
+
         self.token = None
-        self.api_url = BASE_URL + "conn/test.php?token="
+        self.api_base_url = BASE_URL + "conn/test.php?token="
+        self.api_url = ""
+
+        assert logging_level in LOGGING_LEVELS
+        logging.basicConfig(format=logging_format, datefmt=logging_datefmt, level=LOGGING_LEVELS[logging_level], filename=logging_file if logging_file else "",
+                            filemode=logging_mode)
+
+        logging.info("DiscordRobots object has been init.")
 
     def connect(self, token: str) -> bool:
         """
@@ -50,7 +76,7 @@ class DiscordRobots:
             raise TokenIsInvalidException("Token is invalid!")
 
         self.token = token
-        self.api_url += self.token
+        self.api_url = self.api_base_url + self.token
 
         res = requests.get(url=self.api_url, params=dict())
         data = res.json()
@@ -60,7 +86,7 @@ class DiscordRobots:
             logging.error("Failed to connect to DiscordRobots. Reason: Got an invalid Token in DiscordRobots#connect()!")
             raise TokenIsInvalidException("Token is invalid!")
         else:
-            logging.info("Successfully connected to DiscordRobotsAPI")
+            logging.info("Successfully connected to DiscordRobotsAPI with api object")
             return True
 
     def has_voted(self, user_id: str) -> bool:
@@ -90,4 +116,3 @@ class DiscordRobots:
             voted = json.dumps(data["isVoted"], indent=4)
             logging.info("Successfully gathered information about User having already voted!")
             return voted == "true"
- 
